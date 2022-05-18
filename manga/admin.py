3213d5017ba1f35132888a0682d4784d6a5b9e9b
André Ludwig <deugondim88@gmail.com>
2022-05-18 18:47:51 -0300

    add chapter and pages on admin

--- manga/admin.py
+++ manga/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Chapter, PostPage
+
 # Register your models here.
+
+
+class PostPageAdmin(admin.StackedInline):
+    model = PostPage
+
+
+@admin.register(Chapter)
+class ChapterAdmin(admin.ModelAdmin):
+    inlines = [PostPageAdmin]
+
+    class Meta:
+        model = Chapter
--- manga/models.py
+++ manga/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chapter(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class PostPage(models.Model):
+    post = models.ForeignKey(Chapter, default=None, on_delete=models.CASCADE)
+    pages = models.FileField(upload_to='manga/static/manga/pages/')
+
+    def __str__(self):
+        return self.post.title
