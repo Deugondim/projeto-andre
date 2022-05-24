07fb22b105941a7e16dca0b28d5618afea31199e
André Ludwig <deugondim88@gmail.com>
2022-05-24 17:39:41 -0300

    created the links on home page

--- manga/urls.py
+++ manga/urls.py
@@ -1,7 +1,10 @@
+
 from django.urls import path
 
 from . import views
 
+app_name = 'manga'
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.ChapterListView.as_view(), name='home'),
+    path('<int:pk>/', views.PostPageView.as_view(), name='postpage'),
 ]
--- manga/views.py
+++ manga/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+
+from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
+
+from .models import Chapter, PostPage
 
 
-def home(request):
-    return render(request, 'manga/pages/home.html')
+class ChapterListView(ListView):
+
+    model = Chapter
+    template_name = 'manga/pages/home.html'
+    context_object_name = 'chapters'
+
+
+class PostPageView(DetailView):
+
+    model = PostPage
+    template_name = 'manga/pages/chapter.html'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Chapter, pk=self.kwargs.get('pk'))
